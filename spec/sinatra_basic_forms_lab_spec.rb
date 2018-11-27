5766412447137714e4ea317c0836940c4a0d6ee4
Rima Jean <rima.jean@gmail.com>
2018-11-27 17:05:59 +0000

    automatically backed up by learn

--- spec/sinatra_basic_forms_lab_spec.rb
+++ spec/sinatra_basic_forms_lab_spec.rb
@@ -65,6 +65,7 @@ describe App do
       # The \s below will match any whitespace
       expect(page).to have_text(/Name:\s+Butch/i)
       expect(page).to have_text(/Breed:\s+Mastiff/i)
+      binding.pry 
       expect(page).to have_text(/Age:\s+6 months/i)
 
       #now do it again, to be sure it's not hard-coded
