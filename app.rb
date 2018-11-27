5f9b08bc120fdcc65ae6e4c9c59e719cc3e6c174
Rima Jean <rima.jean@gmail.com>
2018-11-27 16:43:10 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    binding.pry
+    # binding.pry
     @new_puppy = Puppy.new(params[:name], params[:breed], params[:age])
     erb :display_puppy
   end
