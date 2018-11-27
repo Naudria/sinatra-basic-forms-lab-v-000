24f1176ee28ef2c7a0d2e10931cf8ee98c3f9466
Rima Jean <rima.jean@gmail.com>
2018-11-27 16:36:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,19 +1,18 @@
 require_relative 'config/environment'
-require_relative 'models/puppy'
 
 class App < Sinatra::Base
 
-get '/' do
-  erb :index
-end
+  get '/' do
+    erb :index
+  end
 
-get '/new' do
-  erb :create_puppy
-end
+  get '/new' do
+    erb :create_puppy
+  end
 
-post '/puppy' do
-  # binding.pry
-    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+  post '/' do
+    @new_pooch = Puppy.new(params[:name], params[:breed], params[:age])
+
     erb :display_puppy
   end
 end
